3d5b8db5d6f90d0f2639c2db9fb1451fd0bfc1f3
Ash <link007angel@gmail.com>
2025-07-01 19:38:49 -0300

    Initial commit: basic CLI word count tool

---
+++ src/main.zig
@@ -0,0 +1,198 @@
+const std = @import("std");
+
+const File = std.fs.File;
+
+const Buffer = struct {
+    str: []u8,
+    len: usize,
+    allocator: std.mem.Allocator,
+
+    const Self = @This();
+
+    fn init(allocator: std.mem.Allocator) Buffer {
+        return .{
+            .str = undefined,
+            .len = 0,
+            .allocator = allocator,
+        };
+    }
+
+    fn deinit(self: *Self) void {
+        self.allocator.free(self.str);
+    }
+};
+
+const Flags = struct {
+    line: bool,
+    word: bool,
+    char: bool,
+
+    const Self = @This();
+
+    fn create() Flags {
+        return .{
+            .line = false,
+            .word = false,
+            .char = false,
+        };
+    }
+
+    fn setDefaultIfFalse(self: *Self) void {
+        if (self.line) return;
+        if (self.word) return;
+        if (self.char) return;
+
+        self.line = true;
+        self.word = true;
+        self.char = true;
+    }
+};
+
+const help =
+    \\Zig Word Count
+    \\
+    \\Usage: zwc [FILE] [OPTION]
+    \\
+    \\CLI utility to count the amount of Lines, Words, and Characters in a file.
+    \\
+    \\Arguments:
+    \\-h                    show this.
+    \\-l                    only show amount of lines
+    \\-w                    only show amount of words
+    \\-c                    only show amount of characters
+    \\
+    \\You can combine -l -w -c into -lwc -clw to show several (show all is default)
+    \\
+    \\Non existing valid arguments will be ignored.
+;
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    if (args.len < 2) {
+        try println("Type: zwc -h for help", .{});
+        return;
+    }
+
+    var file_path: ?[]const u8 = null;
+    var i: usize = 1;
+
+    var flags = Flags.create();
+
+    if (args.len > 2) {
+        while (i < args.len) : (i += 1) {
+            const arg = args[i];
+            if (arg[0] == '-') {
+                for (arg) |char| {
+                    switch (char) {
+                        'h' => {
+                            try println("{s}\n\n", .{help});
+                            return;
+                        },
+                        'l' => flags.line = true,
+                        'w' => flags.word = true,
+                        'c' => flags.char = true,
+                        else => {},
+                    }
+                }
+            } else if (file_path == null) {
+                file_path = arg;
+            } else {
+                try println("Too many positional arguments.\n", .{});
+            }
+        }
+    }
+
+    flags.setDefaultIfFalse();
+
+    if (file_path == null) {
+        try println("Missing file path.\n", .{});
+        return;
+    }
+
+    const cwd = std.fs.cwd();
+    const file = cwd.openFile(file_path.?, .{}) catch |err| switch (err) {
+        error.FileNotFound => {
+            try println("File doesn't exist.", .{});
+            return;
+        },
+        else => {
+            try println("Error: {}", .{err});
+            return err;
+        },
+    };
+    defer file.close();
+
+    try zwc(file, allocator, flags);
+}
+
+fn zwc(file: File, allocator: std.mem.Allocator, flags: Flags) !void {
+    const reader = file.reader();
+
+    var line_count: usize = 0;
+    var word_count: usize = 0;
+    var char_count: usize = 0;
+
+    while (true) {
+        var buffer = Buffer.init(allocator);
+        defer buffer.deinit();
+
+        try readLineDynamic(reader, &buffer, '\n');
+
+        if (buffer.len == 0) break;
+
+        line_count += 1;
+        char_count += buffer.len;
+
+        var words = std.mem.tokenizeScalar(u8, buffer.str[0..buffer.len], ' ');
+
+        while (words.next() != null) {
+            word_count += 1;
+        }
+    }
+
+    if (flags.line) try println("> Line count -- {d}", .{line_count});
+    if (flags.word) try println("> word count -- {d}", .{word_count});
+    if (flags.char) try println("> char count -- {d}", .{char_count});
+}
+
+fn readLineDynamic(reader: anytype, buffer: *Buffer, delimiter: u8) !void {
+    buffer.str = try buffer.allocator.alloc(u8, 64); // initial guess
+    buffer.len = 0;
+
+    while (true) {
+        const byte = reader.readByte() catch |err| switch (err) {
+            error.EndOfStream => break,
+            else => return err,
+        };
+
+        if (buffer.len == buffer.str.len) {
+            const new_buf = try buffer.allocator.realloc(buffer.str, buffer.str.len * 2);
+            buffer.str = new_buf;
+        }
+
+        buffer.str[buffer.len] = byte;
+        buffer.len += 1;
+
+        if (byte == delimiter) break;
+    }
+}
+
+fn println(comptime fmt: []const u8, args: anytype) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print(fmt, args);
+    try stdout.print("\n", .{});
+}
+
+fn print(comptime fmt: []const u8, args: anytype) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print(fmt, args);
+}
+
+fn writeAll(fmt: []const u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.writeAll(fmt);
+}
